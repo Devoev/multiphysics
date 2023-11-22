updated main.ipynb fixed value mz in mesh fixed solve_poisson.py system matrix fixed sign of g matrix in top_mats.py

fit/matrices/top_mats.py
@@ -16,7 +16,7 @@ def create_top_mats(msh: Mesh) -> Tuple[spmatrix, spmatrix, spmatrix]:
     py = create_p_mat(n, msh.ny)
     pz = create_p_mat(n, msh.nz)
 
-    g = bmat([[-px.T, -py.T, -pz.T]]).T
+    g = bmat([[px.T, py.T, pz.T]]).T
     c = bmat([[None, -pz, py], [pz, None, -px], [-py, pz, None]])
     d = bmat([[px, py, pz]])
     return g, c, d
@@ -31,5 +31,5 @@ def create_p_mat(n: int, m: int) -> spmatrix:
 
     rows = np.concatenate((np.arange(n), np.arange(n - m)))
     cols = np.concatenate((np.arange(n), np.arange(m, n)))
-    vals = np.concatenate((-np.ones((n, 1)), np.ones((n - m, 1))))[:, 0]
+    vals = np.concatenate((-np.ones((n,)), np.ones((n - m,))))
     return csr_matrix((vals, (rows, cols)), shape=(n, n))

fit/solver/solve_poisson.py
@@ -1,5 +1,6 @@
 import numpy as np
-from scipy.sparse.linalg import spsolve
+from numpy.linalg import matrix_rank
+from scipy.sparse.linalg import spsolve, norm
 
 from fit.matrices.const_mats import create_p2d_mat
 from fit.matrices.top_mats import create_top_mats
@@ -19,7 +20,7 @@ def solve_poisson(msh: Mesh, f: np.ndarray, mat: float, bc: np.ndarray) -> np.nd
     # Assemble matrix
     g, _, d = create_top_mats(msh)
     const_mat = create_p2d_mat(msh, mat)
-    A = -d @ const_mat @ g
+    A = g.T @ const_mat @ g
 
     # Deflate system
     idx_dof = np.isnan(bc)
@@ -28,6 +29,9 @@ def solve_poisson(msh: Mesh, f: np.ndarray, mat: float, bc: np.ndarray) -> np.nd
 
     b = f[idx_dof] - A[idx_dof,:][:,idx_bc] @ bc
     A = A[idx_dof,:][:,idx_dof]
+
+    print(A.shape)
+    print(matrix_rank(A.toarray()))
 
     # Solve system
     x = np.empty((msh.np,))

main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.sparse.linalg import norm
 
 from fit.matrices.const_mats import create_p2d_mat
 from fit.matrices.top_mats import create_top_mats
@@ -8,14 +9,20 @@ from fit.solver.solve_poisson import solve_poisson
 
 xmesh = np.linspace(0, 10, 10)
 ymesh = np.linspace(0, 10, 10)
-zmesh = np.linspace(0, 10, 2)
+zmesh = np.linspace(0, 10, 5)
 msh = Mesh(xmesh, ymesh, zmesh)
 
-#%%
-bc = np.full(msh.np, np.nan)
-idx_bc = [msh.idx(10, j, 1) for j in range(10)]
-bc[idx_bc] = 10
 
 #%%
-eps = 8.854e-12
-phi = solve_poisson(msh, np.zeros(msh.np), eps, bc)
+g, c, d = create_top_mats(msh)
+print(norm(c@g))
+print(norm(d@c))
+
+# #%%
+# bc = np.full(msh.np, np.nan)
+# idx_bc = [msh.idx(10, j, 1) for j in range(10)]
+# bc[idx_bc] = 10
+#
+# #%%
+# eps = 8.854e-12
+# phi = solve_poisson(msh, np.zeros(msh.np), eps, bc)