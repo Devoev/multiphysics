5956ef0b732e77ea83c2eb7102b31482f81d6c56
Devin <devinbalian@gmail.com>
2023-11-22 16:47:49 +0100

    added solve_poisson.py

---
+++ fit/solver/solve_poisson.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy.sparse.linalg import spsolve
+
+from fit.matrices.const_mats import create_p2d_mat
+from fit.matrices.top_mats import create_top_mats
+from fit.mesh.mesh import Mesh
+
+
+def solve_poisson(msh: Mesh, f: np.ndarray, mat: float, bc: np.ndarray) -> np.ndarray:
+    """
+    Solves the `Poisson` problem.
+    :param msh: Mesh object.
+    :param f: Right hand side vector of size ``np``.
+    :param mat: Material inside the domain.
+    :param bc: Boundary conditions vector of size ``np``. ``NaN`` at DOF indices.
+    :return: Solution vector.
+    """
+
+    # Assemble matrix
+    g, _, d = create_top_mats(msh)
+    const_mat = create_p2d_mat(msh, mat)
+    A = -d @ const_mat @ g
+
+    # Deflate system
+    idx_dof = np.isnan(bc)
+    idx_bc = ~idx_dof
+    bc = bc[idx_bc]
+
+    b = f[idx_dof] - A[idx_dof,:][:,idx_bc] * bc
+    A = A[idx_dof,:][:,idx_dof]
+
+    # Solve system
+    x = np.empty((msh.np,))
+    x[idx_dof] = spsolve(A, b)
+    x[idx_bc] = bc
+    return x
--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from matplotlib import pyplot as plt
 from fit.matrices.const_mats import create_p2d_mat
 from fit.matrices.top_mats import create_top_mats
 from fit.mesh.mesh import Mesh
+from fit.solver.solve_poisson import solve_poisson
 
 xmesh = np.linspace(0, 10, 10)
 ymesh = np.linspace(0, 10, 10)
-zmesh = np.linspace(0, 10, 10)
+zmesh = np.linspace(0, 10, 1)
 msh = Mesh(xmesh, ymesh, zmesh)
 
-g, c, d = create_top_mats(msh)
+#%%
+bc = np.full(msh.np, np.nan)
+idx_bc = [msh.idx(10, j, 1) for j in range(10)]
+bc[idx_bc] = 10
 
-meps = create_p2d_mat(msh, 8.854e-12)
-mmui = create_p2d_mat(msh, 1 / 1.256e-6)
-
-plt.spy(d @ meps @ g, markersize=.1)
-plt.show()
+#%%
+eps = 8.854e-12
+phi = solve_poisson(msh, np.zeros(msh.np), eps, bc)
