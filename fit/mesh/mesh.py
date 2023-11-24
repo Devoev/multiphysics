749719c8543fe80f51c761c1af4cb23e40959280
Devin <devinbalian@gmail.com>
2023-11-24 21:02:45 +0100

    added find_idx function to mesh.py

--- fit/mesh/mesh.py
+++ fit/mesh/mesh.py
@@ -24,5 +24,13 @@ class Mesh:
         """Calculates the canonical index."""
         return i*self.mx + j*self.my + k*self.mz
 
+    def find_idx(self, x: float, y: float, z: float) -> float:
+        """Finds the canonical index nearest to the given physical coordinates."""
+        return self.idx(
+            np.argmin(np.abs(self.xmesh - x)),
+            np.argmin(np.abs(self.ymesh - y)),
+            np.argmin(np.abs(self.zmesh - z))
+        )
+
     def __iter__(self):
         return iter((self.xmesh, self.ymesh, self.zmesh, self.nx, self.ny, self.nz, self.np, self.mx, self.my, self.mz))
