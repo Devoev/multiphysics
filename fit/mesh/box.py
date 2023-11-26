8e72d0aab42c0af698c50c925ed7290a32a4ce53
Devin <devinbalian@gmail.com>
2023-11-26 23:34:42 +0100

    fixed bug in mesh_boxes

--- fit/mesh/box.py
+++ fit/mesh/box.py
@@ -25,23 +25,32 @@ def mesh_boxes(msh: Mesh, boxes: List[Box], mat: float) -> np.ndarray:
     :return: Material array of size (msh.np).
     """
 
-    mat = mat * np.ones(msh.np,)
+    mat = np.full(msh.np, mat)
     for box in boxes:
         xmin, xmax = box.x_idx
         ymin, ymax = box.y_idx
         zmin, zmax = box.z_idx
 
-        dx = xmax - xmin
-        dy = ymax - ymin
-        dz = zmax - zmin
+        idx = []
+        for i in range(xmin, xmax):
+            for j in range(ymin, ymax):
+                for k in range(zmin, zmax):
+                    idx.append(msh.idx(i,j,k))
 
-        x_idx = np.arange(xmin, xmax)
-        y_idx = np.arange(ymin, ymax)
-        z_idx = np.arange(zmin, zmax)
-
-        idx = np.tile(x_idx, (dy,)) + np.tile(y_idx * msh.my, (dx,))
-        idx = np.reshape(idx, (dx*dy))
-        idx = np.tile(idx, (dz,)) + np.tile(z_idx * msh.mz, (dx*dy,))
         mat[idx] = box.value
+
+        # TODO: Fix this
+        # dx = xmax - xmin
+        # dy = ymax - ymin
+        # dz = zmax - zmin
+        #
+        # x_idx = np.arange(xmin, xmax)
+        # y_idx = np.arange(ymin, ymax)
+        # z_idx = np.arange(zmin, zmax)
+        #
+        # idx = np.tile(x_idx, (dy,)) + np.tile(y_idx * msh.my, (dx,))
+        # idx = np.reshape(idx, (dx*dy))
+        # idx = np.tile(idx, (dz,)) + np.tile(z_idx * msh.mz, (dx*dy,))
+        # mat[idx] = box.value
 
     return mat
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from scipy.sparse.linalg import norm
 
 from fit.matrices.const_mats import create_p2d_mat
 from fit.matrices.top_mats import create_top_mats, create_p_mat
+from fit.mesh.box import Box, mesh_boxes
 from fit.mesh.mesh import Mesh
 from fit.solver.solve_poisson import solve_poisson
 
@@ -26,8 +27,28 @@ print(norm(d @ c))
 print(norm(g.T @ c.T))
 
 #%%
-plt.spy(c, markersize=0.1)
-plt.show()
+V0 = 1
+
+bc = np.full(msh.np, np.nan)
+idx_bc_1 = []
+idx_bc_2 = []
+
+for i in range(10):
+    for k in range(2):
+        idx_bc_1.append(msh.idx(0, i, k))
+        idx_bc_2.append(msh.idx(10-1, i, k))
+
+bc[idx_bc_1] = 0
+bc[idx_bc_2] = V0
+
+box_bc_1 = Box(0, (0, 1), (0, msh.ny), (0, msh.nz))
+box_bc_2 = Box(V0, (10-1, 10), (0, msh.ny), (0, msh.nz))
+bc_new = mesh_boxes(msh, [box_bc_1, box_bc_2], np.nan)
+
+print(bc)
+print(bc_new)
+print(np.array_equal(bc, bc_new, equal_nan=True))
+print(bc - bc_new)
 
 # #%%
 # bc = np.full(msh.np, np.nan)
