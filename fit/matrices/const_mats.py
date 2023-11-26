05f5e58cb23706295871f463376aa53470f88127
Devin <devinbalian@gmail.com>
2023-11-26 20:50:30 +0100

    added code for inhomogeneous material in const_mats.py

--- fit/matrices/const_mats.py
+++ fit/matrices/const_mats.py
@@ -1,19 +1,36 @@
-from scipy.sparse import spmatrix, eye
+import numpy as np
+from scipy.sparse import spmatrix, eye, diags, block_diag
 
 from fit.matrices.geo_mats import create_geo_mats
 from fit.matrices.util import spdiag_pinv
 from fit.mesh.mesh import Mesh
 
 
-def create_p2d_mat(msh: Mesh, mat: float) -> spmatrix:
+def create_p2d_mat(msh: Mesh, mat: float | np.ndarray) -> spmatrix:
     """Creates the constitutive matrix from the primary to dual grid.
     :param msh: Mesh object.
-    :param mat: Material parameter for the constitutive relation.
+    :param mat: Material parameter for the constitutive relation. Either a single value or an array of size (msh.np).
     """
 
-    ds, _, _, dat = create_geo_mats(msh)
-    const_mat = mat * eye(3 * msh.np)
-    return dat @ const_mat @ spdiag_pinv(ds)
+    _, _, _, nx, ny, nz, n, mx, my, mz = msh
+    ds, _, da, dat = create_geo_mats(msh)
+
+    if isinstance(mat, float):
+        dmat = mat * eye(3*n)
+    elif isinstance(mat, np.ndarray):
+        diag = np.ones((4,n))
+
+        x_av = diags(diag, (0, -my, -mz, -my-mz), (n,n))
+        y_av = diags(diag, (0, -mz, -mx, -mz-mx), (n,n))
+        z_av = diags(diag, (0, -mx, -my, -my-mx), (n,n))
+        av = block_diag([x_av, y_av, z_av])
+
+        mat = np.concatenate([mat, mat, mat])
+        dmat = diags(0.25*spdiag_pinv(dat) @ av @ da @ mat, 0, (3*n,3*n))
+    else:
+        pass
+
+    return dat @ dmat @ spdiag_pinv(ds)
 
 
 def create_d2p_mat(msh: Mesh, mat: float) -> spmatrix:
