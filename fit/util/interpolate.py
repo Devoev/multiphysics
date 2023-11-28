1b4a3cdd7953c06d6008f85d4f6d6b2bc4a7ba67
Devin <devinbalian@gmail.com>
2023-11-28 18:36:26 +0100

    add interpolation functions plot interpolated field in main.ipynb

--- fit/util/interpolate.py
+++ fit/util/interpolate.py
@@ -5,47 +5,67 @@ from fit.matrices.util import pinv
 from fit.mesh.mesh import Mesh
 
 
-def interpolate_field(msh: Mesh, vec: np.ndarray):
+def interpolate_field(msh: Mesh, vec: np.ndarray) -> np.ndarray:
+    """
+    Interpolates the given field strength ``vec``.
+    :param msh: Mesh object.
+    :param vec: Field array of size ``(3*msh.np)``.
+    :return: Interpolated field array ``(3*msh.np)``.
+    """
 
     _, _, _, nx, ny, nz, n, mx, my, mz = msh
     ds, *_ = create_geo_mats(msh)
 
-    print(vec)
     vec = pinv(ds) @ vec
-    print(vec)
-    # eEdge = eBow[:, 0] * nullInvVector(DSDiag[0:3 * n_p]);
-    # eEdgeX = eEdge[0:n_p];
-    # eEdgeY = eEdge[n_p:2 * n_p];
-    # eEdgeZ = eEdge[2 * n_p:3 * n_p];
-    #
-    # # Interpolation des E-Feldes
-    # eX = zeros(n_p);
-    # eY = zeros(n_p);
-    # eZ = zeros(n_p);
-    # for k in range(n_z):
-    #     for j in range(n_y):
-    #         for i in range(n_x):
-    #             # Berechnung des kanonischen Index
-    #             n = i * M_x + j * M_y + k * M_z;
-    #
-    #             # X-Richtung
-    #             if i != 0 and i != n_x - 1:
-    #                 eX[n] = (eEdgeX[n - M_x] * DSDiag[n] + eEdgeX[n] * DSDiag[n - M_x]) / (
-    #                         DSDiag[n] + DSDiag[n - M_x]);
-    #
-    #             # Y-Richtung
-    #             if j != 0 and j != n_y - 1:
-    #                 eY[n] = (eEdgeY[n - M_y] * DSDiag[n_p + n] + eEdgeY[n] * DSDiag[n_p + n - M_y]) / (
-    #                         DSDiag[n_p + n] + DSDiag[n_p + n - M_y]);
-    #
-    #             # Z-Richtung
-    #             if k != 0 and k != n_z - 1:
-    #                 eZ[n] = (eEdgeZ[n - M_z] * DSDiag[2 * n_p + n] + eEdgeZ[n] * DSDiag[2 * n_p + n - M_z]) / (
-    #                         DSDiag[2 * n_p + n] + DSDiag[2 * n_p + n - M_z]);
-    #
-    # eField = hstack([eX, eY, eZ]);
-    #
-    # # meshP ist das reduzierte Gitter, das heißt ohne 1. und letzten Punkt
-    # meshP = mesh(self.xmesh[2:-2], self.ymesh[2:-2], self.zmesh[2:-2]);
+    return vec
 
-    # return meshP, eField
+
+def interpolate_flux(msh: Mesh, vec: np.ndarray) -> np.ndarray:
+    """
+    Interpolates the given flux density ``vec``.
+    :param msh: Mesh object.
+    :param vec: Flux array of size ``(3*msh.np)``.
+    :return: Interpolated flux array ``(3*msh.np)``.
+    """
+
+    _,_,da,_ = create_geo_mats(msh)
+
+    vec = pinv(da) @ vec
+    return vec
+
+# eEdge = eBow[:, 0] * nullInvVector(DSDiag[0:3 * n_p]);
+# eEdgeX = eEdge[0:n_p];
+# eEdgeY = eEdge[n_p:2 * n_p];
+# eEdgeZ = eEdge[2 * n_p:3 * n_p];
+#
+# # Interpolation des E-Feldes
+# eX = zeros(n_p);
+# eY = zeros(n_p);
+# eZ = zeros(n_p);
+# for k in range(n_z):
+#     for j in range(n_y):
+#         for i in range(n_x):
+#             # Berechnung des kanonischen Index
+#             n = i * M_x + j * M_y + k * M_z;
+#
+#             # X-Richtung
+#             if i != 0 and i != n_x - 1:
+#                 eX[n] = (eEdgeX[n - M_x] * DSDiag[n] + eEdgeX[n] * DSDiag[n - M_x]) / (
+#                         DSDiag[n] + DSDiag[n - M_x]);
+#
+#             # Y-Richtung
+#             if j != 0 and j != n_y - 1:
+#                 eY[n] = (eEdgeY[n - M_y] * DSDiag[n_p + n] + eEdgeY[n] * DSDiag[n_p + n - M_y]) / (
+#                         DSDiag[n_p + n] + DSDiag[n_p + n - M_y]);
+#
+#             # Z-Richtung
+#             if k != 0 and k != n_z - 1:
+#                 eZ[n] = (eEdgeZ[n - M_z] * DSDiag[2 * n_p + n] + eEdgeZ[n] * DSDiag[2 * n_p + n - M_z]) / (
+#                         DSDiag[2 * n_p + n] + DSDiag[2 * n_p + n - M_z]);
+#
+# eField = hstack([eX, eY, eZ]);
+#
+# # meshP ist das reduzierte Gitter, das heißt ohne 1. und letzten Punkt
+# meshP = mesh(self.xmesh[2:-2], self.ymesh[2:-2], self.zmesh[2:-2]);
+
+# return meshP, eField
--- main.py
+++ main.py
@@ -1,66 +1,15 @@
 import numpy as np
-from matplotlib import pyplot as plt
-from scipy.sparse import diags, block_diag, coo_matrix
-from scipy.sparse.linalg import norm
 
-from fit.matrices.const_mats import create_p2d_mat
-from fit.matrices.geo_mats import create_geo_mats
-from fit.matrices.top_mats import create_top_mats, create_p_mat
-from fit.matrices.util import pinv
-from fit.mesh.box import Box, mesh_boxes
 from fit.mesh.mesh import Mesh
-from fit.solver.solve_poisson import solve_poisson
+from fit.util.interpolate import interpolate_field
 
-xmesh = np.linspace(0, 1, 2)
-ymesh = np.linspace(0, 1, 2)
+xmesh = np.linspace(0, 1, 7)
+ymesh = np.linspace(0, 1, 7)
 zmesh = np.linspace(0, 1, 2)
 msh = Mesh(xmesh, ymesh, zmesh)
 
 
-#%%
-n = msh.np
-px = create_p_mat(n, msh.mx)
-py = create_p_mat(n, msh.my)
-pz = create_p_mat(n, msh.mz)
-
-#%%
-g, c, d = create_top_mats(msh)
-print(norm(c @ g))
-print(norm(d @ c))
-print(norm(g.T @ c.T))
-
-#%%
-ds, dst, da, dat = create_geo_mats(msh)
-ds = ds.toarray()
-dst = dst.toarray()
-da = da.toarray()
-dat = dat.toarray()
-
-#%%
-_, _, _, nx, ny, nz, n, mx, my, mz = msh
-diag = np.ones((4,n))
-x_av = diags(diag, (0, -my, -mz, -my-mz), (n,n))
-y_av = diags(diag, (0, -mz, -mx, -mz-mx), (n,n))
-z_av = diags(diag, (0, -mx, -my, -mx-my), (n,n))
-av = block_diag([x_av, y_av, z_av]).toarray()
-
-plt.spy(av, markersize=.1)
-plt.show()
-
-#%%
-sig = 1
-sig_arr = np.full(msh.np, sig)
-m1 = create_p2d_mat(msh, sig_arr).toarray()
-m2 = create_p2d_mat(msh, sig).toarray()
-
-plt.figure()
-plt.spy(m1, markersize=.1)
-plt.title("Array")
-plt.show()
-
-plt.figure()
-plt.spy(m2, markersize=.1)
-plt.title("Scalar")
-plt.show()
-
-print(np.linalg.norm(m1 - m2))
+E0 = 1
+ebow = E0 * np.ones((3*msh.np,))
+print(ebow)
+print(interpolate_field(msh, ebow))
