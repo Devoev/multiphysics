0b68d787c1086d904c749afab10e56957eabc5e3
devin <devinbalian@gmail.com>
2023-12-10 15:54:34 +0100

    change imports scipy in util.py and const_mats.py

--- fit/matrices/const_mats.py
+++ fit/matrices/const_mats.py
@@ -1,14 +1,14 @@
 import numbers
 
 import numpy as np
-from scipy.sparse import spmatrix, eye, diags, block_diag
+import scipy.sparse as sp
 
 from fit.matrices.geo_mats import create_geo_mats
 from fit.matrices.util import pinv
 from fit.mesh.mesh import Mesh
 
 
-def create_p2d_mat(msh: Mesh, mat: float | np.ndarray) -> spmatrix:
+def create_p2d_mat(msh: Mesh, mat: float | np.ndarray) -> sp.spmatrix:
     """Creates the constitutive matrix from the primary to dual grid.
     :param msh: Mesh object.
     :param mat: Material parameter for the constitutive relation. Either a single value or an array of size (msh.np).
@@ -18,29 +18,29 @@ def create_p2d_mat(msh: Mesh, mat: float | np.ndarray) -> spmatrix:
     ds, _, da, dat = create_geo_mats(msh)
 
     if isinstance(mat, numbers.Number):
-        dmat = eye(3*n) * mat
+        dmat = sp.eye(3*n) * mat
     elif isinstance(mat, np.ndarray):
         diag = np.ones((4,n))
 
-        x_av = diags(diag, (0, -my, -mz, -my-mz), (n,n))
-        y_av = diags(diag, (0, -mz, -mx, -mz-mx), (n,n))
-        z_av = diags(diag, (0, -mx, -my, -mx-my), (n,n))
-        av = block_diag([x_av, y_av, z_av])
+        x_av = sp.diags(diag, (0, -my, -mz, -my-mz), (n,n))
+        y_av = sp.diags(diag, (0, -mz, -mx, -mz-mx), (n,n))
+        z_av = sp.diags(diag, (0, -mx, -my, -mx-my), (n,n))
+        av = sp.block_diag([x_av, y_av, z_av])
 
         mat = np.concatenate([mat, mat, mat])
-        dmat = diags(0.25 * pinv(dat) @ av @ da @ mat, 0, (3 * n, 3 * n))
+        dmat = sp.diags(0.25 * pinv(dat) @ av @ da @ mat, 0, (3 * n, 3 * n))
     else:
         raise Exception("not implemented")
 
     return dat @ dmat @ pinv(ds)
 
 
-def create_d2p_mat(msh: Mesh, mat: float) -> spmatrix:
+def create_d2p_mat(msh: Mesh, mat: float) -> sp.spmatrix:
     """Creates the constitutive matrix from the dual to primary grid.
     :param msh: Mesh object.
     :param mat: Material parameter for the constitutive relation.
     """
 
     _, dst, da, _ = create_geo_mats(msh)
-    const_mat = mat * eye(3 * msh.np)
+    const_mat = mat * sp.eye(3 * msh.np)
     return dst @ const_mat @ pinv(da)
--- fit/matrices/util.py
+++ fit/matrices/util.py
@@ -1,8 +1,8 @@
-from scipy.sparse import spmatrix, csr_matrix
+import scipy.sparse as sp
 
 
-def pinv(mat: spmatrix) -> spmatrix:
+def pinv(mat: sp.spmatrix) -> sp.spmatrix:
     """Computes the pseudo inverse of the given sparse diagonal matrix ``mat``."""
 
     mat = mat.tocoo()
-    return csr_matrix((1 / mat.data, (mat.row, mat.col)), shape=mat.shape)
+    return sp.coo_matrix((1 / mat.data, (mat.row, mat.col)), shape=mat.shape)
