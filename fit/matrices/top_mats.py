e7af6ce65ed7d07e4b6159607aada850fed56fa2
devin <devinbalian@gmail.com>
2023-12-11 15:01:18 +0100

    update imports and types in top_mats.py

--- fit/matrices/top_mats.py
+++ fit/matrices/top_mats.py
@@ -1,12 +1,12 @@
 from typing import Tuple
 
 import numpy as np
-from scipy.sparse import csr_matrix, spmatrix, bmat
+import scipy.sparse as sp
 
 from fit.mesh.mesh import Mesh
 
 
-def create_top_mats(msh: Mesh) -> Tuple[spmatrix, spmatrix, spmatrix]:
+def create_top_mats(msh: Mesh) -> Tuple[sp.coo_matrix, sp.coo_matrix, sp.coo_matrix]:
     """Creates the topological matrices for the given ``msh``.
     :return: The ``g``, ``c`` and ``d`` matrices.
     """
@@ -16,13 +16,13 @@ def create_top_mats(msh: Mesh) -> Tuple[spmatrix, spmatrix, spmatrix]:
     py = create_p_mat(n, msh.my)
     pz = create_p_mat(n, msh.mz)
 
-    g = bmat([[px.T, py.T, pz.T]]).T
-    c = bmat([[None, -pz, py], [pz, None, -px], [-py, px, None]])
-    d = bmat([[px, py, pz]])
+    g = sp.bmat([[px.T, py.T, pz.T]]).T
+    c = sp.bmat([[None, -pz, py], [pz, None, -px], [-py, px, None]])
+    d = sp.bmat([[px, py, pz]])
     return g, c, d
 
 
-def create_p_mat(n: int, m: int) -> spmatrix:
+def create_p_mat(n: int, m: int) -> sp.coo_matrix:
     """Creates a discrete differentiation matrix.
 
     :param n: Number of grid points.
@@ -32,4 +32,4 @@ def create_p_mat(n: int, m: int) -> spmatrix:
     rows = np.concatenate((np.arange(n), np.arange(n - m)))
     cols = np.concatenate((np.arange(n), np.arange(m, n)))
     vals = np.concatenate((-np.ones((n,)), np.ones((n - m,))))
-    return csr_matrix((vals, (rows, cols)), shape=(n, n))
+    return sp.coo_matrix((vals, (rows, cols)), shape=(n, n))
